e356c12abda6f814e312f755b31dd664844cb457
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-14 20:58:09 +0300

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true, exclusion: { in: ['True Facts'] }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  
+    validate :title_is_clickbait
+  
+    private
+  
+    def title_is_clickbait
+      if title == "You Won't Believe These True Facts"
+        errors.add(:title, "cannot be clickbait")
+      end
+    end
+  end
+  
